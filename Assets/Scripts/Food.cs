d31ddb1b9164aa14ac14fa9072ae89bc5dc4b486
Mana <manakouwenberg@gmail.com>
2020-10-07 11:10:46 +0200

    Food moves yay

--- Assets/Scripts/Food.cs
+++ Assets/Scripts/Food.cs
@@ -4,6 +4,23 @@ using UnityEngine;
 
 public class Food : MonoBehaviour
 {
+    Vector3 destination;
+    public float speed;
+    private void Start()
+    {
+        destination = new Vector3(Random.Range(-18, 18), 0.5f, Random.Range(-8, 8));
+    }
+
+    private void Update()
+    {
+        if(Vector3.Distance(destination, transform.position) < 0.5f)
+        {
+            destination = new Vector3(Random.Range(-18, 18), 0.5f, Random.Range(-8, 8));
+        }
+        Vector3 dir = (destination - transform.position).normalized;
+        transform.position += dir * speed * Time.deltaTime;
+    }
+
     public void GetHit()
     {
         StartCoroutine(GetHitC());
